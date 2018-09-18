f4756e3623fab9555ca5c5e487a49fa176368843
Daniel Lee <chococloudy@gmail.com>
2018-09-18 09:46:17 -0600

    longest most frequenet done

--- hw/hw1/frequency.py
+++ hw/hw1/frequency.py
@@ -10,7 +10,12 @@ frequency = Counter(without_space)
 print("Frequent: " + str(frequency.most_common(2)) )
 sortedwords = sorted(without_space,key=len)
 print("Longest: " + str(sortedwords[-1]))
-longestfrequent = sorted(frequency, key = lambda t: len(t[0]), reverse=True)
-#print(longestfrequent[1])
-#print(line)
+#longestfrequent = sorted(frequency, key = lambda t: len(t[0]), reverse=True)
+longestfrequent = frequency.most_common()
+longestfrequent.sort(key = lambda t: len(t[0]), reverse=True)
+for tup in longestfrequent:
+    if(tup[1] > 24 ):
+        print("Longest that occurs more than 25 times: " + tup[0])
+        break
+
 f.close()
