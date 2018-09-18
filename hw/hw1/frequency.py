1ba16f6fcb88d8dd299ccf648a39c236e0cb50ed
Daniel Lee <chococloudy@gmail.com>
2018-09-17 21:47:20 -0600

    Longest word added

--- hw/hw1/frequency.py
+++ hw/hw1/frequency.py
@@ -7,6 +7,10 @@ for line in lines:
     temp = line.split()
     without_space += temp
 frequency = Counter(without_space)
-print(frequency.most_common(2))
+print("Frequent: " + str(frequency.most_common(2)) )
+sortedwords = sorted(without_space,key=len)
+print("Longest: " + str(sortedwords[-1]))
+longestfrequent = sorted(frequency, key = lambda t: len(t[0]), reverse=True)
+#print(longestfrequent[1])
 #print(line)
 f.close()
