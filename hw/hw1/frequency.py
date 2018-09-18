ccf3f785cb3c62393d99dee238ad4b4ade84592f
Daniel Lee <chococloudy@gmail.com>
2018-09-17 21:27:16 -0600

    Getting frequency

--- hw/hw1/frequency.py
+++ hw/hw1/frequency.py
@@ -1,9 +1,12 @@
 from collections import Counter
 
-
 f = open("ulysses.txt","r",encoding='UTF-8')
+without_space = []
 lines = f.readlines()
 for line in lines:
-    print(line)
+    temp = line.split()
+    without_space += temp
+frequency = Counter(without_space)
+print(frequency.most_common(2))
 #print(line)
 f.close()
