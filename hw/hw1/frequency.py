0c31baae807f6a11d927ed2f73c58e3ff8933fee
Hanum Lee <daniel@Hanums-MacBook-Pro.local>
2018-09-21 14:28:40 -0600

    trans3 decoded

--- hw/hw1/frequency.py
+++ hw/hw1/frequency.py
@@ -1,5 +1,7 @@
 from collections import Counter
 from itertools import cycle
+import string
+import re
 
 f = open("ulysses.txt","r",encoding='UTF-8')
 without_space = []
@@ -28,6 +30,8 @@ transfile = []
 pineindex = textwithspace.find("Pineapple rock")
 #print(textwithspace.find("[ 8 ]"))
 
+#https://stackoverflow.com/questions/29408173/byte-operations-xor-in-python
+
 with open("transmission1", "rb") as b:
     byte = b.read(1)
     while byte:
@@ -42,6 +46,9 @@ key = [ chr(ord(a) ^ b) for (a,b) in zip(transtext, transfile) ]
 print(key)
 predictedkey = "snowboard"
 
+#section 13
+
+#key for transmission 1 and 2 is snowboard
 
 transmission2 = []
 #t2 = open("transmission2","rb")
@@ -56,8 +63,43 @@ trans2 = [ chr((a) ^ ord(b)) for (a,b) in zip(transmission2, cycle(predictedkey)
 
 print(trans2)
 
+#https://stackoverflow.com/questions/265960/best-way-to-strip-punctuation-from-a-string-in-python
+
+question3 = textwithspace
+question3.strip()
+exclude = set(string.punctuation)
+#table = string.maketrans("","")
+#question3.translate(table, string.punctuation)
+quest3 = ''.join(ch for ch in question3 if ch not in exclude)
+quest3 = ''.join(quest3.split())
+quest3 = quest3.upper()
 
 
+t3 = open("transmission3.txt","r")
+trans3 = ""
+lines = t3.readlines()
+for line in lines:
+    trans3 += line
 
-#key for transmission 1 and 2 is snowboard
-#print(len(transfile))
+#print(trans3)
+
+trans3t =  ''.join(chr(ord(a) ^ ord(b)) for (a,b) in zip(quest3, cycle(trans3)))
+#indexthe = [m.start() for m in re.finditer('the', trans3t)]
+#print(indexthe)
+'''for oc in indexthe:
+    print(oc)
+    print(trans3t[oc:oc+100])'''
+#print(trans3t[indexthe:indexthe + 100])
+#question3.replace("\n","")
+#print("Trans3" + str(trans3t))
+t3.close()
+#print(quest3)
+
+for n in range(1, len(trans3t)):
+    substr_counter = Counter(trans3t[i: i+n] for i in range(len(trans3t) - n))
+    phrase, count = substr_counter.most_common(1)[0]
+    if count == 1:      # early out for trivial cases
+        break
+    print ('Size: %3d:  Occurrences: %3d  Phrase: %r' % (n, count, phrase))
+
+
