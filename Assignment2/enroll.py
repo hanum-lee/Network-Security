ee28a97a68d5a789879a25d7713dd112b78250ba
hanum-lee <chococloudy@gmail.com>
2018-10-25 10:30:25 -0600

    Reading database added

--- Assignment2/enroll.py
+++ Assignment2/enroll.py
@@ -1,18 +1,31 @@
 from argon2 import PasswordHasher
+import sys
+import string
+
 
 ph = PasswordHasher()
+#db = open("database.txt","a",encoding="UTF-8")
 
-db = open("database.txt","a",encoding="UTF-8")
+database = []
+with open("database.txt") as fp:
+    for i in fp.readlines():
+        tmp = i.split()
+        try:
+            database.append((tmp[0], tmp[1]))
+            #result.append((eval(tmp[0]), eval(tmp[1])))
+        except:pass
+
+#with open("database.txt") as db:
+#    database = [tuple(map(string, i.split(","))) for i in db]
+#database = result
 with open("words.txt") as dc:
     lines = dc.readlines()
 
 words = [x.strip() for x in lines]
 
-
-
-
+print(sys.argv[0],sys.argv[1],sys.argv[2])
 print(words[1])
-
+print(database[1])
 
 hash = ph.hash("Test")
 print(hash)
@@ -27,4 +40,4 @@ print(ph.check_needs_rehash(hash))
 
 
 
-db.close()
+#db.close()
