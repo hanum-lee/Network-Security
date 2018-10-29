45ae9e917e464187608cc2ef1265edf8e003c877
Daniel Lee <chococloudy@gmail.com>
2018-10-28 18:43:15 -0600

    checking passwords done in regular expression

--- Assignment2/enroll.py
+++ Assignment2/enroll.py
@@ -1,6 +1,7 @@
 from argon2 import PasswordHasher
 import sys
 import string
+import re
 
 
 ph = PasswordHasher()
@@ -21,6 +22,20 @@ with open("words.txt") as dc:
 
 words = [x.strip() for x in lines]
 
+for x in words:
+    x.lower();
+
+#par = re.compile[]
+
+def checkDic(inputwd):
+    try:
+        words.index(inputwd)
+        print("Password in dictionary")
+        return True
+    except:
+        return False
+
+
 def checkValidID(inputid):
 
     if(len([item for item in database if item[0] == inputid]) > 0):
@@ -30,21 +45,52 @@ def checkValidID(inputid):
 
 
 def checkValidPassword(inputpw):
+#    try:
+#        float(inputpw)
+#        return False
+#    except ValueError:
+#        pass
+    if(re.match(r'^([\s\d]+)$',inputpw)):
+        print("Only numbers")
+        return False
+#    try:
+#        words.index(inputpw)
+#        print("Password in dictionary")
+#        return False
+#    except:
+        #return True
+#        pass
+    if(checkDic(inputpw)):
+        return False
     try:
-        float(inputpw)
-        return False
-    except ValueError:
+        m = re.match(r"(\d+)(\w+)", inputpw)
+#        print("Group1:", m.group(1))
+#        print("Groupt2:" ,m.group(2))
+#        print("Num first")
+        if(checkDic(m.group(2))):
+            return False
+    except:
         pass
-    if(words.index("inputpw") > 0):
-        return False
+    try:
+        m = re.match(r"(\w+)(\d+)", inputpw)
+#        print("Group1:", m.group(1))
+#        print("Groupt2:" ,m.group(2))
+#        print("Word First")
+        if(checkDic(m.group(1))):
+            return False
+    except:
+        pass
+    return True
+    #if():
 
-    
-    
+
+
 
 #Basic setup
 
 
-print(checkValidID(sys.argv[1]))
+print("Check id:",checkValidID(sys.argv[1]))
+print("Check pw:",checkValidPassword(sys.argv[2]))
 
 print(sys.argv[0],sys.argv[1],sys.argv[2])
 print(words[1])
@@ -59,6 +105,3 @@ try:
 except Exception as e:
     print(e)
 print(ph.check_needs_rehash(hash))
-
-
-
