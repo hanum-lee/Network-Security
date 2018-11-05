da69a2ae2faebe5139442f680642eb53e3fd6e1d
Daniel Lee <chococloudy@gmail.com>
2018-11-04 22:56:10 -0700

    amount of website works added

--- hw/hw3/cert.py
+++ hw/hw3/cert.py
@@ -9,15 +9,26 @@ res = req.urlopen(url)
 
 soup = BeautifulSoup(res, "html.parser")
 sites = soup.select(" div.td.DescriptionCell > p > a")
-truesites = []
+sitesurl = []
 for site in sites:
-    #truesites.append(str(site))
     temp = str(site)
-    #print(temp)
     m = re.search(r'(?<=siteinfo/)(.*)(?=\")', temp)
-    print("Site:",m.group(1))
+    urlsite = "https://www." + m.group(1)
+    sitesurl.append(urlsite)
+    #print("url:",urlsite)
+existsite = []
+for site in sitesurl:
+    try:
+        request = requests.get(site)
+        if request.status_code == 200:
+            print("Exist:",site)
+            existsite.append(site)
+        else:
+            print("Not Exist:",site)
+    except Exception as e:
+        print(e)
 
-
+print("Amount of website works:",len(existsite))
 #for site in truesites:
 #    m = re.match(r'(?<=\>)(.*?)(?=\<)', str(site))
 #    print("Site:",m)
