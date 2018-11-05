d4d28fae080e968d93d379444c2d02b2caeec76b
Daniel Lee <chococloudy@gmail.com>
2018-11-04 21:11:02 -0700

    parsing site

--- hw/hw3/cert.py
+++ hw/hw3/cert.py
@@ -1,11 +1,24 @@
 from bs4 import BeautifulSoup
 import urllib.request as req
+import re
+import requests
+
 
 url = "https://www.alexa.com/topsites/category/Top/Games"
 res = req.urlopen(url)
 
 soup = BeautifulSoup(res, "html.parser")
 sites = soup.select(" div.td.DescriptionCell > p > a")
+truesites = []
 for site in sites:
-    print("Site:",site)
+    #truesites.append(str(site))
+    temp = str(site)
+    #print(temp)
+    m = re.search(r'(?<=siteinfo/)(.*)(?=\")', temp)
+    print("Site:",m.group(1))
+
+
+#for site in truesites:
+#    m = re.match(r'(?<=\>)(.*?)(?=\<)', str(site))
+#    print("Site:",m)
 #print("Site",site)
